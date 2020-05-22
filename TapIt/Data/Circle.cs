3b8af23ff0b9c684cd262d0daeee756d47ccaba9
David Nemecek <40146720160278@litec.ac.at>
2020-05-22 15:30:26 +0200

    Projektdateien hinzufügen.

---
+++ TapIt/Data/Circle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Drawing;
+
+namespace TapIt.Data
+{
+    public class Circle
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Size { get; set; }
+        public string Fill { get; set; }
+
+        public Circle(int x, int y, int r, string fill)
+        {
+            X = x;
+            Y = y;
+            Size = r;
+            Fill = fill;
+        }
+
+        /// <summary>
+        /// Create a Circle with Random Parameters
+        /// </summary>
+        /// <param name="s">Screen</param>
+        /// <returns>Returns a Circle with Random Parameters</returns>
+        public static Circle RandomCircleGenerator(Screen s)
+        {
+            var rand = new Random();
+            return new Circle(rand.Next(s.Width),
+                rand.Next(s.Height),
+                rand.Next(s.MinCircleSize, s.MaxCircleSize),
+                "#" + rand.Next(0,256).ToString("X2") + rand.Next(0, 256).ToString("X2") + rand.Next(0, 256).ToString("X2"));
+        }
+    }
+}
---
+++ TapIt/Data/Screen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace TapIt.Data
+{
+    /// <summary>
+    /// Properties of the Screen including Circle Dimensions and Circles
+    /// </summary>
+    public class Screen
+    {
+        public List<Circle> Circles = new List<Circle>();
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int MaxCircleSize => (Height / 150) * (Width / 150);
+        public int MinCircleSize => (Height / 300) * (Width / 300);
+
+        public Screen(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+    }
+}
